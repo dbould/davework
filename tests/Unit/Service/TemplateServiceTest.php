4814e141d9a7edd3db76e268167c98f26aeb19f8
dave <dave@bould.net>
2017-11-11 11:55:33 +0000

    Added tests for fetching new templates

--- tests/Unit/Service/TemplateServiceTest.php
+++ tests/Unit/Service/TemplateServiceTest.php
@@ -29,4 +29,135 @@ TESTFACTORY;
 
         $this->assertEquals($expected, $actual);
     }
+
+    public function testGetFactoryFunctionalTestTemplate()
+    {
+        $service = $this->getContainer()->get(TemplateService::class);
+
+        $expected = <<<'FACTORYTEST'
+<?php
+namespace %s;
+
+use %s;
+use Tests\SlimTestCase;
+
+class %s extends SlimTestCase
+{
+    public function testItReturnsAnInstance()
+    {
+        $actual = $this->getContainer()->get(%s::class);
+
+        $this->assertInstanceOf(%s::class, $actual);
+    }
+}
+
+FACTORYTEST;
+
+        $actual = $service->getTemplate('FactoryFunctionalTest');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetControllerTemplate()
+    {
+        $service = $this->getContainer()->get(TemplateService::class);
+
+        $expected =  <<<'CONTROLLER'
+<?php
+namespace %s;
+
+class %s
+{
+    public function action(Request $request, Response $response, array $args)
+    {
+        
+        return $response;
+    }
+}
+
+CONTROLLER;
+
+        $actual = $service->getTemplate('Controller');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetControllerFunctionalTestTemplate()
+    {
+        $service = $this->getContainer()->get(TemplateService::class);
+
+        $expected = <<<'CONTROLLERTEST'
+<?php
+namespace %s;
+
+class %sTest extends BaseTestCase
+{
+    /**
+     * Test that the index route returns a rendered response containing the text 'SlimFramework' but not a greeting
+     */
+    public function testAction()
+    {
+        $response = $this->runApp('GET', '/');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('SlimFramework', (string)$response->getBody());
+        $this->assertNotContains('Hello', (string)$response->getBody());
+    }
+}
+
+CONTROLLERTEST;
+
+        $actual = $service->getTemplate('ControllerFunctionalTest');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetServiceTemplate()
+    {
+        $service = $this->getContainer()->get(TemplateService::class);
+
+        $expected = <<<'SERVICE'
+<?php
+namespace %s;
+
+class %s
+{
+    public function __construct()
+    {
+        
+    }
+}
+
+SERVICE;
+
+        $actual = $service->getTemplate('Service');
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGetServiceFunctionalTestTemplate()
+    {
+        $service = $this->getContainer()->get(TemplateService::class);
+
+        $expected = <<<'SERVICETEST'
+<?php
+namespace %s;
+
+use %s;
+use Tests\SlimTestCase;
+
+class %s extends SlimTestCase
+{
+    public function test()
+    {
+        $this->assertEquals(true, false);
+    }
+}
+
+SERVICETEST;
+
+        $actual = $service->getTemplate('ServiceFunctionalTest');
+
+        $this->assertEquals($expected, $actual);
+    }
 }

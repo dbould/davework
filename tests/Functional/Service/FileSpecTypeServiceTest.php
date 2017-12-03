80ff2e1f718802e8a8c02767471c39e5938e8189
dave <dave@bould.net>
2017-12-03 22:02:20 +0000

    Add service test stubs

---
+++ tests/Functional/Service/FileSpecTypeServiceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Functional\Service;
+
+class FileSpecTypeServiceTest
+{
+    public function testGetRootDirectory()
+    {
+
+    }
+
+    public function testGetTypeFromFileName()
+    {
+
+    }
+
+    public function testGetTopLevelNamespace()
+    {
+        
+    }
+}

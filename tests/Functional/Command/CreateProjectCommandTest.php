b06fa48ef0f31d8a6b274023ce4e5bd7d47008e3
dave <dave@bould.net>
2017-12-06 21:38:21 +0000

    Removed uneeded file removals from command test File removals not relevant

--- tests/Functional/Command/CreateProjectCommandTest.php
+++ tests/Functional/Command/CreateProjectCommandTest.php
@@ -9,19 +9,6 @@ class CreateProjectCommandTest extends SlimTestCase
 {
     public function testExecuteCreatesSlimProject()
     {
-        $fileNames = [
-            __DIR__ . '/../../TestFiles/src/Service/MooMooService.php',
-            __DIR__ . '/../../TestFiles/tests/Functional/Service/MooMooServiceTest.php',
-            __DIR__ . '/../../TestFiles/src/Factory/MooMooServiceFactory.php',
-            __DIR__ . '/../../TestFiles/tests/Functional/Factory/MooMooServiceFactoryTest.php',
-        ];
-
-        foreach ($fileNames as $fileName) {
-            if (file_exists($fileName)) {
-                unlink($fileName);
-            }
-        }
-
         $command = new CreateProjectCommand();
         $commandTest = new CommandTester($command);
 

d7dab0e24fc2947fabeb7ac9f99fa1e67994f79e
dave <dave@bould.net>
2017-12-10 19:36:40 +0000

    Fixed regex variable name to correctly reflect intent

--- create-phar.php
+++ create-phar.php
@@ -1,10 +1,9 @@
 <?php
-
 $buildRoot = __DIR__;
 
 $phar = new Phar($buildRoot . '/build/davework.phar', 0, 'davework.phar');
 
-$exclude = '/^(?=(.*src|.*bin|.*vendor|.*bootstrap))(?!(.*tests))(.*)$/i';
+$include = '/^(?=(.*src|.*bin|.*vendor|.*bootstrap))(?!(.*tests))(.*)$/i';
 
-$phar->buildFromDirectory($buildRoot, $exclude);
-$phar->setStub($phar->createDefaultStub("bin/davework"));
+$phar->buildFromDirectory($buildRoot, $include);
+$phar->setStub("#!/usr/bin/env php\n" . $phar->createDefaultStub("bin/davework"));

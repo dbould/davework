c8388a569a399b110be6d873f9e2b44a2475f4ae
dave <dave@bould.net>
2017-12-17 22:46:05 +0000

    More Travis debugging

--- tests/Functional/Service/CreateSlimProjectServiceTest.php
+++ tests/Functional/Service/CreateSlimProjectServiceTest.php
@@ -20,9 +20,19 @@ class CreateSlimProjectServiceTest extends SlimTestCase
         $process2->run();
         foreach ($process2 as $type => $data) {
             if ($process2::OUT === $type) {
-                echo "\nRead from stdout: ".$data;
+                echo "\nRead from stdout2: ".$data;
             } else { // $process::ERR === $type
-                echo "\nRead from stderr: ".$data;
+                echo "\nRead from stderr2: ".$data;
+            }
+        }
+
+        $process3 = new Process('ls -la ' . __DIR__ . '/../../TestFiles/Project/slim-skeleton');
+        $process3->run();
+        foreach ($process3 as $type => $data) {
+            if ($process3::OUT === $type) {
+                echo "\nRead from stdout3: ".$data;
+            } else { // $process::ERR === $type
+                echo "\nRead from stderr3: ".$data;
             }
         }
 
@@ -30,6 +40,16 @@ class CreateSlimProjectServiceTest extends SlimTestCase
 
         $newProcess = new Process('ls -la ' . __DIR__ . '/../../TestFiles/project');
         $newProcess->run();
+
+        foreach ($newProcess as $type => $data) {
+            if ($newProcess::OUT === $type) {
+                echo "\nRead from stdout4: ".$data;
+            } else { // $process::ERR === $type
+                echo "\nRead from stderr4: ".$data;
+            }
+        }
+        $newProcess->run();
+die();
 
         $this->assertEquals(true, $actual);
 

534317041277d1727ab1aa23dfb8c5fefc0d9e58
dave <dave@bould.net>
2017-12-17 22:14:00 +0000

    More Travis debugging

--- tests/Functional/Service/CreateSlimProjectServiceTest.php
+++ tests/Functional/Service/CreateSlimProjectServiceTest.php
@@ -18,6 +18,16 @@ class CreateSlimProjectServiceTest extends SlimTestCase
 
         $actual = file_exists(__DIR__ . '/../../TestFiles/project/slim-skeleton/composer.json');
 
+        $process2 = new Process('chmod -R 777 ' . __DIR__ . '/../../TestFiles/project/slim-skeleton');
+        $process2->run();
+        foreach ($process2 as $type => $data) {
+            if ($process2::OUT === $type) {
+                echo "\nRead from stdout: ".$data;
+            } else { // $process::ERR === $type
+                echo "\nRead from stderr: ".$data;
+            }
+        }
+
         $newProcess = new Process('ls -la ' . __DIR__ . '/../../TestFiles/project/slim-skeleton');
         $newProcess->run();
         foreach ($newProcess as $type => $data) {

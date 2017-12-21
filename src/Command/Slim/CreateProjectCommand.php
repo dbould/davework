9f3981e19af68d87713e01df0dcfd32657a0c750
dave <dave@bould.net>
2017-12-21 22:10:23 +0000

    Remove unused import

--- src/Command/Slim/CreateProjectCommand.php
+++ src/Command/Slim/CreateProjectCommand.php
@@ -1,7 +1,6 @@
 <?php
 namespace Davework\Command\Slim;
 
-use Davework\Service\CreateSlimProjectService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
